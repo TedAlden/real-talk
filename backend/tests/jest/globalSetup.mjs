d664a6cb91cc829e974c64b146f869b3b6a1c485
justan-kj <jtjtjt703@gmail.com>
2025-02-18 19:11:07 +0000

    Add start and dev scripts to package.json; update test command to use Jest
    Renamed test folder to tests
    
    Fixed typo in filename
    
    Changed file extension to fix jest compaitibility
    Jest doesn't work very well out of the box with ESM
    
    Update Jest configuration for ESM support
    
    Fix MongoDB connection URI in global setup for Jest tests
    
    Cahnged coverage reporters for jest to text only
    Prevent creation of new files in project directory

--- backend/tests/jest/globalSetup.mjs
+++ backend/tests/jest/globalSetup.mjs
@@ -2,12 +2,14 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import { MongoClient } from "mongodb";
 
 async function globalSetup() {
-  const database_name = "test";
   const instance = await MongoMemoryServer.create();
   const uri = instance.getUri();
+  process.env.MONGO_TEST_DB = "TestDB";
   process.env.MONGO_TEST_URI = uri.slice(0, uri.lastIndexOf("/"));
   global.__MONGOINSTANCE = instance;
-  const client = new MongoClient(process.env.MONGO_URI + "/" + database_name);
+  const client = new MongoClient(
+    process.env.MONGO_TEST_URI + "/" + process.env.MONGO_TEST_DB
+  );
 
   await client.connect();
   await client.db().dropDatabase();
