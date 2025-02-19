0b63615aa6c60deebbd59dba337d018917073734
justan-kj <jtjtjt703@gmail.com>
2025-02-19 02:35:22 +0000

    Fixed incorrect end point uri Changed from /user/ to /auth/

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -4,10 +4,9 @@ import app from "../src/app";
 describe("User API", () => {
   test("should register a new user", async () => {
     const res = await request(app)
-      .post("/users/register")
+      .post("/auth/register")
       .send({ username: "testuser", password: "password123" });
-
     expect(res.statusCode).toBe(201);
-    expect(res.body).toHaveProperty("message", "User registered successfully");
+    expect(res.body).toHaveProperty("message", "User registered successfully.");
   });
 });
