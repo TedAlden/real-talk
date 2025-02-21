dc3d507ebaa120103df7debad566fa8fc5018bf4
justan-kj <jtjtjt703@gmail.com>
2025-02-21 18:16:21 +0000

    Implement login/logout functionality with cookie management

--- frontend/src/components/LoginWindow.jsx
+++ frontend/src/components/LoginWindow.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { loginUser } from "../api/userService";
+import Cookies from "js-cookie";
 
 function LoginWindow() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [alert, setAlert] = useState("");
+  const [token, setToken] = useState(Cookies.get("authToken"));
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,53 +17,76 @@ function LoginWindow() {
     const response = await loginUser(submittedUser);
 
     if (response.success !== false) {
-      setAlert("Login successful!");
+      setAlert("");
+      Cookies.set("authToken", response.data.token, {
+        expires: 7,
+        secure: true,
+        sameSite: "strict",
+      });
+      setToken(response.data.token);
     } else {
       console.log(response);
       setAlert("Login failed! " + response.error);
     }
   };
 
+  const handleLogout = () => {
+    Cookies.remove("authToken");
+    setToken(null);
+  };
+
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 2fr",
-          gap: "0.75em",
-          textAlign: "right",
-        }}
-      >
-        <label>Username:</label>
-        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+    <>
+      {token ? (
+        <form onSubmit={handleLogout}>
+          <p>You are logged in</p>
+          <button style={{ width: "96px" }}>Logout</button>
+        </form>
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "1fr 2fr",
+              gap: "0.75em",
+              textAlign: "right",
+            }}
+          >
+            <label>Username:</label>
+            <input type="text" onChange={(e) => setUsername(e.target.value)} />
 
-        <label>Password:</label>
-        <input type="password" onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <div
-        style={{
-          background: "red",
-          color: "white",
-          padding: "0.5em",
-          width: "100%",
-          margin: "1em",
-          minHeight: "2em",
-          borderRadius: "5px",
-          visibility: alert ? "visible" : "hidden", // Keeps space reserved
-        }}
-      >
-        {alert}
-      </div>
+            <label>Password:</label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div
+            style={{
+              background: "red",
+              color: "white",
+              padding: "0.5em",
+              width: "100%",
+              margin: "1em",
+              minHeight: "2em",
+              borderRadius: "5px",
+              visibility: alert ? "visible" : "hidden",
+            }}
+          >
+            {alert}
+          </div>
 
-      <button style={{ width: "96px", marginTop: "1em" }}>Login</button>
-    </form>
+          <button style={{ width: "96px", marginTop: "1em" }}>Login</button>
+        </form>
+      )}
+    </>
   );
 }
 
