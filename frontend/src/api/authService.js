374514f7381be1879664471f4ae8ae2b8bf9db5a
justan-kj <jtjtjt703@gmail.com>
2025-02-24 22:46:42 +0000

    Improve error messages  in authService

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -13,7 +13,9 @@ async function loginUser(user) {
     if (error.response) {
       //If error has a response, it means the request reached the backend but it sent back an error.
       console.error("Backend error:", error);
-      return { success: false, error: error.message };
+      const code = error.response.status;
+      const message = error.response.data.error;
+      return { success: false, error: `Status ${code}: ${message}` };
     } else {
       //Otherwise, it's an error with the request
       console.error("Error:", error);
@@ -34,7 +36,9 @@ async function registerUser(user) {
     if (error.response) {
       //If error has a response, it means the request reached the backend but it sent back an error.
       console.error("Backend error:", error);
-      return { success: false, error: error.message };
+      const code = error.response.status;
+      const message = error.response.data.error;
+      return { success: false, error: `Status ${code}: ${message}` };
     } else {
       //Otherwise, it's an error with the request
       console.error("Error:", error);
@@ -54,7 +58,9 @@ async function sendResetEmail(user) {
     if (error.response) {
       //If error has a response, it means the request reached the backend but it sent back an error.
       console.error("Backend error:", error);
-      return { success: false, error: error.message };
+      const code = error.response.status;
+      const message = error.response.data.error;
+      return { success: false, error: `Status ${code}: ${message}` };
     } else {
       //Otherwise, it's an error with the request
       console.error("Error:", error);
@@ -74,7 +80,9 @@ async function verifyEmail(user) {
     if (error.response) {
       //If error has a response, it means the request reached the backend but it sent back an error.
       console.error("Backend error:", error);
-      return { success: false, error: error.message };
+      const code = error.response.status;
+      const message = error.response.data.error;
+      return { success: false, error: `Status ${code}: ${message}` };
     } else {
       //Otherwise, it's an error with the request
       console.error("Error:", error);
