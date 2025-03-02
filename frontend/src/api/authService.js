e3e9caa0a470fe4afaf970b461e1340ee9adbb91
Ted <t.aldentempleman@gmail.com>
2025-03-02 12:14:17 +0000

    Refactor authService with error handler

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -1,139 +1,85 @@
 import axiosInstance from "./axios";
 
-//Logs in the given user
+/**
+ * Handle axios API errors.
+ *
+ * @param {Object} error The error object from axios.
+ * @returns {Object} An object with a success boolean and an error message.
+ */
+const handleError = (error) => {
+  if (error.response) {
+    // If error has a response, it means the request reached the backend but it
+    // sent back an error.
+    console.error("Backend error:", error);
+    const code = error.response.status;
+    const message = error.response.data.error;
+    return { success: false, error: `Status ${code}: ${message}` };
+  } else {
+    // Otherwise, it's an error with the request
+    console.error("Error:", error);
+    return { success: false, error: error };
+  }
+};
+
 export async function loginUser(user) {
   try {
-    //Makes a POST request to the "/users/login" endpoint. Auth stuff is usually POST because it's more secure.
     const response = await axiosInstance.post("/auth/login", user);
-    //Log the response for debugging purposes and return the response
-    //The response should contain a token which we can use to authenticate the user on protected endpoints.
     console.log(response);
     return response;
   } catch (error) {
-    if (error.response) {
-      //If error has a response, it means the request reached the backend but it sent back an error.
-      console.error("Backend error:", error);
-      const code = error.response.status;
-      const message = error.response.data.error;
-      return { success: false, error: `Status ${code}: ${message}` };
-    } else {
-      //Otherwise, it's an error with the request
-      console.error("Error:", error);
-      return { success: false, error: error };
-    }
+    return handleError(error);
   }
 }
 
 export async function registerUser(user) {
   try {
-    //Makes a POST request to the "/users/register" endpoint. Auth stuff is usually POST because it's more secure.
     const response = await axiosInstance.post("/auth/register", user);
-    //Log the response for debugging purposes and return the response.
-    //Usually the response is the info of the newly registered user or maybe a token so they can be logged in immediately.
     console.log(response);
     return response;
   } catch (error) {
-    if (error.response) {
-      //If error has a response, it means the request reached the backend but it sent back an error.
-      console.error("Backend error:", error);
-      const code = error.response.status;
-      const message = error.response.data.error;
-      return { success: false, error: `Status ${code}: ${message}` };
-    } else {
-      //Otherwise, it's an error with the request
-      console.error("Error:", error);
-      return { success: false, error: error };
-    }
+    handleError(error);
   }
 }
 
 export async function sendResetEmail(user) {
   try {
-    //Makes a POST request to the "/users/register" endpoint. Auth stuff is usually POST because it's more secure.
     const response = await axiosInstance.post("/auth/forgot-password", user);
-    //Log the response for debugging purposes and return the response.
     console.log(response);
     return response;
   } catch (error) {
-    if (error.response) {
-      //If error has a response, it means the request reached the backend but it sent back an error.
-      console.error("Backend error:", error);
-      const code = error.response.status;
-      const message = error.response.data.error;
-      return { success: false, error: `Status ${code}: ${message}` };
-    } else {
-      //Otherwise, it's an error with the request
-      console.error("Error:", error);
-      return { success: false, error: error };
-    }
+    handleError(error);
   }
 }
 
 export async function verifyEmail(user) {
   try {
-    //Makes a POST request to the "/users/register" endpoint. Auth stuff is usually POST because it's more secure.
     const response = await axiosInstance.post("/auth/verify-email", user);
-    //Log the response for debugging purposes and return the response.
     console.log(response);
     return response;
   } catch (error) {
-    if (error.response) {
-      //If error has a response, it means the request reached the backend but it sent back an error.
-      console.error("Backend error:", error);
-      const code = error.response.status;
-      const message = error.response.data.error;
-      return { success: false, error: `Status ${code}: ${message}` };
-    } else {
-      //Otherwise, it's an error with the request
-      console.error("Error:", error);
-      return { success: false, error: error };
-    }
+    handleError(error);
   }
 }
 
 export async function resetPassword(user) {
   try {
-    //Makes a POST request to the "/users/register" endpoint. Auth stuff is usually POST because it's more secure.
     const response = await axiosInstance.post("/auth/reset-password", user);
-    //Log the response for debugging purposes and return the response.
     console.log(response);
     return response;
   } catch (error) {
-    if (error.response) {
-      //If error has a response, it means the request reached the backend but it sent back an error.
-      console.error("Backend error:", error);
-      const code = error.response.status;
-      const message = error.response.data.error;
-      return { success: false, error: `Status ${code}: ${message}` };
-    } else {
-      //Otherwise, it's an error with the request
-      console.error("Error:", error);
-      return { success: false, error: error };
-    }
+    handleError(error);
   }
 }
 
 export async function verifyOTP(token, otp) {
   try {
-    //Makes a POST request to the "/users/register" endpoint. Auth stuff is usually POST because it's more secure.
     const response = await axiosInstance.post("/auth/verify-otp", {
       token,
       otp,
     });
-    //Log the response for debugging purposes and return the response.
     console.log(response);
     return response;
   } catch (error) {
-    if (error.response) {
-      //If error has a response, it means the request reached the backend but it sent back an error.
-      console.error("Backend error:", error);
-      const code = error.response.status;
-      const message = error.response.data.error;
-      return { success: false, error: `Status ${code}: ${message}` };
-    } else {
-      //Otherwise, it's an error with the request
-      console.error("Error:", error);
-      return { success: false, error: error };
-    }
+    handleError(error);
   }
 }
