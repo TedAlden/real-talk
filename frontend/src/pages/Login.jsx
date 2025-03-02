c57c5a273c17a8a56c2802e27785553b0ce22be0
Ted <t.aldentempleman@gmail.com>
2025-03-02 23:09:50 +0000

    Update login form - slight style change

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -84,10 +84,13 @@ function Login() {
             <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
               <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
                 <h1 className="text-xl leading-tight font-bold tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Login
+                  Sign in to your RealTalk account
                 </h1>
-                <form className="mx-auto max-w-lg" onSubmit={handleLogin}>
-                  <div className="mb-5">
+                <form
+                  className="mx-auto max-w-lg space-y-6"
+                  onSubmit={handleLogin}
+                >
+                  <div className="">
                     <label
                       htmlFor="username"
                       className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
@@ -99,10 +102,11 @@ function Login() {
                       type="text"
                       id="username"
                       className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                      placeholder="username"
                       required
                     />
                   </div>
-                  <div className="mb-5">
+                  <div className="">
                     <label
                       htmlFor="password"
                       className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
@@ -114,26 +118,36 @@ function Login() {
                       type="password"
                       id="password"
                       className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                      placeholder="••••••••"
                       required
                     />
                   </div>
 
-                  <div className="mb-5 flex items-start">
-                    <div className="flex h-5 items-center">
-                      <input
-                        id="remember"
-                        type="checkbox"
-                        value=""
-                        className="h-4 w-4 rounded-sm border border-gray-300 bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
-                      />
+                  <div className="flex items-start">
+                    <div className="flex items-start">
+                      <div className="flex h-5 items-center">
+                        <input
+                          id="remember"
+                          type="checkbox"
+                          value=""
+                          className="h-4 w-4 rounded-sm border border-gray-300 bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
+                        />
+                      </div>
+                      <label
+                        htmlFor="remember"
+                        className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                      >
+                        Remember me
+                      </label>
                     </div>
-                    <label
-                      htmlFor="remember"
-                      className="ms-2 text-sm text-gray-500 dark:text-gray-400"
+                    <Link
+                      to="/forgot-password"
+                      className="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
                     >
-                      Remember me
-                    </label>
+                      Forgot password?
+                    </Link>
                   </div>
+
                   <button
                     type="submit"
                     className="w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -143,20 +157,20 @@ function Login() {
                   <div className="">
                     <p
                       id="helper-text-explanation"
-                      className="mt-5 text-sm text-gray-500 dark:text-gray-400"
+                      className="text-sm text-gray-500 dark:text-gray-400"
                     >
-                      Forgot your password? Reset it{" "}
+                      Not registered?{" "}
                       <Link
-                        to="/forgot-password"
+                        to="/register"
                         className="font-medium text-blue-600 hover:underline dark:text-blue-500"
                       >
-                        here
+                        Create an account
                       </Link>
                       .
                     </p>
                   </div>
                   {Object.keys(alertMessage).length > 0 && (
-                    <div className="mt-5">
+                    <div className="">
                       <Alert
                         type={alertMessage.type}
                         title={alertMessage.title}
