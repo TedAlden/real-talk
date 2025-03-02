95ca9abf857c74b7664030d02b73cca83a9f5760
Ted <t.aldentempleman@gmail.com>
2025-03-02 19:15:22 +0000

    Add tailwindcss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import "dotenv/config";
 
@@ -7,5 +8,5 @@ const PORT = process.env.VITE_PORT || 5002;
 //vite.dev/config
 export default defineConfig({
   server: { port: PORT },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
