babb595f7738a2202ff6b73d992ad2b4049e2878
justan-kj <jtjtjt703@gmail.com>
2025-03-15 21:29:53 +0000

    Refactor follower tests to use createTestUsers utility and improve clarity

--- backend/tests/followers.test.js
+++ backend/tests/followers.test.js
@@ -1,6 +1,7 @@
 import request from "supertest";
 import app from "../src/app.js";
 import { connectDB, closeDB } from "../src/database/connection.js";
+import { createTestUsers } from "./testUtils.js";
 
 describe("Follower functionality", () => {
   let db;
@@ -9,20 +10,14 @@ describe("Follower functionality", () => {
   beforeAll(async () => {
     db = await connectDB();
     // Insert test users
-    testUsers = await db.collection("users").insertMany([
-      {
-        username: "follower1",
-        email: "follower1@test.com",
-      },
-      {
-        username: "follower2",
-        email: "follower2@test.com",
-      },
-      {
-        username: "followed1",
-        email: "followed1@test.com",
-      },
-    ]);
+    const createdUsers = await createTestUsers(db, 7);
+    testUsers = {
+      celeb: createdUsers.users[0], // User with many followers
+      normal: createdUsers.users[1], // User with normal activity
+      followerA: createdUsers.users[2], // Follows many people
+      followerB: createdUsers.users[3], // Follows only celebs
+      unfollower: createdUsers.users[4], // User who will unfollow someone
+    };
   });
 
   afterAll(async () => {
@@ -38,8 +33,8 @@ describe("Follower functionality", () => {
   describe("POST /api/users/follows", () => {
     test("should create a new follow relationship", async () => {
       const res = await request(app).post(`/api/users/follows`).send({
-        follower_id: testUsers.insertedIds[0],
-        followed_id: testUsers.insertedIds[2],
+        follower_id: testUsers.followerA._id,
+        followed_id: testUsers.celeb._id,
       });
       expect(res.status).toBe(200);
       expect(res.body).toHaveProperty("acknowledged", true);
@@ -47,70 +42,62 @@ describe("Follower functionality", () => {
     });
   });
 
-  // describe("GET /api/users/:id/followers", () => {
-  //   test("should get all followers of a user", async () => {
-  //     // Create some follows first
-  //     await db.collection("followers").insertMany([
-  //       {
-  //         follower_id: testUsers.insertedIds[0],
-  //         followed_id: testUsers.insertedIds[2],
-  //       },
-  //       {
-  //         follower_id: testUsers.insertedIds[1],
-  //         followed_id: testUsers.insertedIds[2],
-  //       },
-  //     ]);
+  describe("GET /api/users/:id/followers", () => {
+    test("should get all followers of a user", async () => {
+      await db.collection("followers").insertMany([
+        {
+          follower_id: testUsers.followerA._id,
+          followed_id: testUsers.celeb._id,
+        },
+        {
+          follower_id: testUsers.followerB._id,
+          followed_id: testUsers.celeb._id,
+        },
+      ]);
 
-  //     console.debug(testUsers.insertedIds[2]);
+      const res = await request(app).get(
+        `/api/users/${testUsers.celeb._id}/followers?viewer_id=${testUsers.celeb._id}`
+      );
 
-  //     const res = await request(app).get(
-  //       `/api/users/${testUsers.insertedIds[2]}/followers?viewer_id=${testUsers.insertedIds[2]}`
-  //     );
-  //     console.log("Response status:", res.status);
-  //     console.log("Response body:", res.body);
-  //     expect(res.status).toBe(200);
-  //     expect(Array.isArray(res.body)).toBe(true);
-  //     expect(res.body).toHaveLength(2);
-  //   });
-  // });
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      expect(res.body).toHaveLength(2);
+    });
+  });
 
-  // describe("GET /api/users/:id/followed", () => {
-  //   test("should get all users followed by a user", async () => {
-  //     // Create follows first
-  //     await db.collection("followers").insertMany([
-  //       {
-  //         follower_id: testUsers.insertedIds[0],
-  //         followed_id: testUsers.insertedIds[1],
-  //       },
-  //       {
-  //         follower_id: testUsers.insertedIds[0],
-  //         followed_id: testUsers.insertedIds[2],
-  //       },
-  //     ]);
+  describe("GET /api/users/:id/followed", () => {
+    test("should get all users followed by a user", async () => {
+      await db.collection("followers").insertMany([
+        {
+          follower_id: testUsers.followerA._id,
+          followed_id: testUsers.celeb._id,
+        },
+        {
+          follower_id: testUsers.followerA._id,
+          followed_id: testUsers.normal._id,
+        },
+      ]);
 
-  //     const res = await request(app).get(
-  //       `/api/users/${testUsers.insertedIds[0]}/followed?viewer_id=${testUsers.insertedIds[0]}`
-  //     );
+      const res = await request(app).get(
+        `/api/users/${testUsers.followerA._id}/followed?viewer_id=${testUsers.followerA._id}`
+      );
 
-  //     console.log("Response status:", res.status);
-  //     console.log("Response body:", res.body);
-
-  //     expect(res.status).toBe(200);
-  //     expect(Array.isArray(res.body)).toBe(true);
-  //     expect(res.body).toHaveLength(2);
-  //   });
-  // });
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      expect(res.body).toHaveLength(2);
+    });
+  });
 
   describe("DELETE /api/users/:follower_id/unfollow/:followed_id", () => {
     test("should remove a follow relationship", async () => {
       // Create follow first
       await db.collection("followers").insertOne({
-        follower_id: testUsers.insertedIds[0],
-        followed_id: testUsers.insertedIds[2],
+        follower_id: testUsers.unfollower._id,
+        followed_id: testUsers.celeb._id,
       });
 
       const res = await request(app).delete(
-        `/api/users/${testUsers.insertedIds[0]}/unfollow/${testUsers.insertedIds[2]}`
+        `/api/users/${testUsers.unfollower._id}/unfollow/${testUsers.celeb._id}`
       );
 
       expect(res.status).toBe(200);
