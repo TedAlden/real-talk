6c00b0d0211c3d5beb44818dae3f39f84dd41c85
Ted <t.aldentempleman@gmail.com>
2025-03-16 17:08:15 +0000

    Refactor user settings page auth handling

--- frontend/src/pages/UserSettings.jsx
+++ frontend/src/pages/UserSettings.jsx
@@ -7,7 +7,7 @@ import _ from "lodash";
 import deepDiff from "../util/deepDiff";
 import deepCopy from "../util/deepCopy";
 
-import { updateUser, getUserById } from "../api/userService.js";
+import { updateUser } from "../api/userService.js";
 import { convertImageBase64 } from "../util/image.js";
 import useAuth from "../hooks/useAuth.js";
 
@@ -40,7 +40,6 @@ function UserSettings() {
   const auth = useAuth();
   const [formData, setFormData] = useState({});
   const [alertMessage, setAlertMessage] = useState("");
-  const [userId, setUserId] = useState(null);
   const [loading, setLoading] = useState(true);
   const [loggedIn, setLoggedIn] = useState(false);
 
@@ -51,23 +50,19 @@ function UserSettings() {
   // to populate the form fields.
 
   useEffect(() => {
-    auth
-      .getUser()
-      .then((user) => {
-        getUserById(user._id).then((response) => {
-          if (response.success !== false) {
-            originalData.current = deepCopy(response.data);
-            setFormData(response.data);
-            setLoggedIn(true);
-            setUserId(user._id);
-            setLoading(false);
-          }
+    if (auth.loggedIn) {
+      auth
+        .getUser()
+        .then((user) => {
+          originalData.current = deepCopy(user);
+          setFormData(user);
+          setLoggedIn(true);
+          setLoading(false);
+        })
+        .catch(() => {
           setLoading(false);
         });
-      })
-      .catch(() => {
-        setLoading(false);
-      });
+    }
   }, [auth]);
 
   const handleProfilePictureChange = async (e) => {
@@ -94,7 +89,6 @@ function UserSettings() {
     e.preventDefault();
     const newUser = {
       ...formData,
-      _id: userId,
     };
 
     // Update password, if confirm password is matching
@@ -112,7 +106,7 @@ function UserSettings() {
     // Only submit the fields that were changed, since this is an HTTP
     // PATCH request.
     const changes = deepDiff(originalData.current, newUser);
-    const response = await updateUser(userId, changes);
+    const response = await updateUser(originalData.current._id, changes);
 
     if (response.success !== false) {
       setAlertMessage({
