ecf5ed7f2199de1592ab1a2066d691589a5c480d
justan-kj <jtjtjt703@gmail.com>
2025-04-20 17:58:21 +0100

    Add validation for post content in updatePostById route

--- backend/src/routes/posts.routes.js
+++ backend/src/routes/posts.routes.js
@@ -26,7 +26,7 @@ postsRouter.post(
 );
 postsRouter.get("/", getPostsByQuery);
 postsRouter.get("/:id", getPostById);
-postsRouter.patch("/:id", updatePostById);
+postsRouter.patch("/:id", useValidators("post_content"), updatePostById);
 postsRouter.delete("/:id", deletePostById);
 
 // Posts :: likes
