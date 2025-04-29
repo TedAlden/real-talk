2f429a9fcfadcaebd3e7020d5fb8a66ee8dd0824
SnBkSpy <rhickson94@gmail.com>
2025-04-29 23:16:21 +0100

    Welcome message will only display with username if username is found

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,17 +7,22 @@ function Home() {
   return (
     // Print welcome message
     <div>
-      <h1 className="my-5 text-2xl font-bold text-gray-900 dark:text-white">
-        Welcome back, {auth?.user?.username}
-      </h1>
+      {auth?.user?.username ? (
+        <h1 className="my-5 text-2xl font-bold text-gray-900 dark:text-white">
+          Welcome back, {auth.user.username}
+        </h1>
+      ) : (
+        <h1 className="my-5 text-2xl font-bold text-gray-900 dark:text-white">
+          Welcome!
+        </h1>
+      )}
+
       {/* Display home image */}
       <div>
         <img src={HomeBG} alt="home" className="h-auto w-full" />
-        <div className="inset-0 flex items-center justify-center text-4xl text-white">
-        </div>
+        <div className="inset-0 flex items-center justify-center text-4xl text-white"></div>
       </div>
-      <p className="text-4xl text-gray-900 dark:text-white">
-      </p>
+      <p className="text-4xl text-gray-900 dark:text-white"></p>
     </div>
   );
 }
