1968043a7cd9d5b32f083b420da1659787c2a95b
Dimitris Bagkos <61548645+Farmakis2000@users.noreply.github.com>
2025-04-30 00:50:59 +0100

    Feature: Added collapsible theme toggle and appropriate light/dark colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,55 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthProvider from "./context/AuthProvider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient();
+  import "./App.css";
+  import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+  import AuthProvider from "./context/AuthProvider";
+  import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+  const queryClient = new QueryClient();
 
-import PublicLayout from "./layouts/PublicLayout";
-import PrivateLayout from "./layouts/PrivateLayout";
+  import PublicLayout from "./layouts/PublicLayout";
+  import PrivateLayout from "./layouts/PrivateLayout";
 
-import Home from "./pages/Home";
-import Landing from "./pages/Landing";
-import ForgotPassword from "./pages/ForgotPassword";
-import VerifyUser from "./pages/VerifyUser";
-import ResetPassword from "./pages/ResetPassword";
-import UserSettings from "./pages/UserSettings";
-import EnterOTP from "./pages/EnterOTP";
-import UserProfile from "./pages/UserProfile";
-import Followers from "./pages/Followers";
-import Following from "./pages/Following";
-import SinglePost from "./pages/SinglePost";
+  import Home from "./pages/Home";
+  import Landing from "./pages/Landing";
+  import ForgotPassword from "./pages/ForgotPassword";
+  import VerifyUser from "./pages/VerifyUser";
+  import ResetPassword from "./pages/ResetPassword";
+  import UserSettings from "./pages/UserSettings";
+  import EnterOTP from "./pages/EnterOTP";
+  import UserProfile from "./pages/UserProfile";
+  import Followers from "./pages/Followers";
+  import Following from "./pages/Following";
+  import SinglePost from "./pages/SinglePost";
 
-function App() {
-  return (
-    <div className="rt-app bg-gray-50 dark:bg-gray-900">
-      <Router>
-        <AuthProvider>
-          <QueryClientProvider client={queryClient}>
-            <div className="container min-w-full">
-              <Routes>
-              <Route element={<PublicLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/landing" element={<Landing />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/verify-email" element={<VerifyUser />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-              </Route>
+  function App() {
+    return (
+      <div className="rt-app bg-gray-50 dark:bg-gray-900">
+        <Router>
+          <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+              <div className="container min-w-full">
+                <Routes>
+                <Route element={<PublicLayout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/landing" element={<Landing />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                  <Route path="/verify-email" element={<VerifyUser />} />
+                  <Route path="/reset-password" element={<ResetPassword />} />
+                </Route>
 
-              <Route element={<PrivateLayout />}>
-                <Route path="/settings" element={<UserSettings />} />
-                <Route path="/enter-otp" element={<EnterOTP />} />
-                <Route path="/profile/:id" element={<UserProfile />} />
-                <Route path="/user/:id/followers" element={<Followers />} />
-                <Route path="/user/:id/following" element={<Following />} />
-                <Route path="/post/:id" element={<SinglePost />} />
-              </Route>
-              </Routes>
-            </div>
-          </QueryClientProvider>
-        </AuthProvider>
-      </Router>
-    </div>
-  );
-}
+                <Route element={<PrivateLayout />}>
+                  <Route path="/settings" element={<UserSettings />} />
+                  <Route path="/enter-otp" element={<EnterOTP />} />
+                  <Route path="/profile/:id" element={<UserProfile />} />
+                  <Route path="/user/:id/followers" element={<Followers />} />
+                  <Route path="/user/:id/following" element={<Following />} />
+                  <Route path="/post/:id" element={<SinglePost />} />
+                </Route>
+                </Routes>
+              </div>
+            </QueryClientProvider>
+          </AuthProvider>
+        </Router>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
--- frontend/src/components/private/Sidebar.jsx
+++ frontend/src/components/private/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { MoreVertical, ChevronLast, ChevronFirst } from "lucide-react";
 import { useContext, createContext, useState } from "react";
+import { DarkThemeToggle } from "flowbite-react";
 
 const SidebarContext = createContext()
 
@@ -8,20 +9,30 @@ export default function Sidebar({ children }) {
   
   return (
     <aside className="h-screen">
-      <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+      <nav className="h-full flex flex-col bg-white dark:bg-gray-800 border-r shadow-sm">
         <div className="p-4 pb-2 flex justify-between items-center">
           <img
             src="https://img.logoipsum.com/243.svg"
             className={`overflow-hidden transition-all ${
               expanded ? "w-32" : "w-0"
             }`}
-            alt=""
           />
+          <div
+            className={`overflow-hidden transition-all flex items-center ${
+                expanded ? "w-auto ml-2" : "w-0"
+              }`}>
+            <DarkThemeToggle />
+          </div>
+
           <button
             onClick={() => setExpanded((curr) => !curr)}
-            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+            className="p-1.5 rounded-lg bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600"
           >
-            {expanded ? <ChevronFirst /> : <ChevronLast />}
+            {expanded ? (
+                <ChevronFirst className="text-gray-500 dark:text-gray-300" /> 
+            ) : (
+                <ChevronLast className="text-gray-500 dark:text-gray-300" />
+            )}
           </button>
         </div>
 
