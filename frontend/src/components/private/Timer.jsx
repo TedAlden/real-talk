7dd78dcc524fae4573e972b44f586608cd9d0404
SnBkSpy <rhickson94@gmail.com>
2025-04-30 21:09:20 +0100

    Changes to horizontal centering of timer text

--- frontend/src/components/private/Timer.jsx
+++ frontend/src/components/private/Timer.jsx
@@ -19,18 +19,23 @@ function Timer() {
     // onTimeRunout: () => setIsLogin(false),
   });
 
-  const progressLabel = ((totalTimeInSeconds - timeRemaining) / totalTimeInSeconds) * 100;
+  const progressLabel =
+    100 - ((totalTimeInSeconds - timeRemaining) / totalTimeInSeconds) * 100;
 
   const handleLogin = () => setIsLogin(true);
   const handleLogout = () => setIsLogin(false);
 
   return (
-    <>
+    <div>
+      <div className="flex justify-center">
+        <div className="pointer-events-none absolute dark:text-white">
+          {timerMinutes}:{String(timerSeconds).padStart(2, "0")}
+        </div>
+      </div>
       <Progress progress={progressLabel} size="xl" />
-      <div className="absolute flex inset-y-5 inset-x-80 justify-center dark:text-white pointer-events-none">{timerMinutes}:{String(timerSeconds).padStart(2, "0")}</div>
-      {/* <Button onClick={resetCountdownTimer}></Button> */}
-      {/* <Button onClick={handleLogin}>Start Timer</Button> */}
-      </>
+      {/* <Button onClick={resetCountdownTimer}></Button>
+      <Button onClick={handleLogin}>Start Timer</Button> */}
+    </div>
   );
 }
 
