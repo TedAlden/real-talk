56d6ba1eaeb1d28ebd9b7206dadf4aa6309cf435
Dimitris Bagkos <61548645+Farmakis2000@users.noreply.github.com>
2025-04-30 21:34:54 +0100

    Cleaning: Removed yellow bar denoting public layout

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -5,9 +5,6 @@ export default function PublicLayout() {
   return (
     <>
       <PublicNavbar />
-      <div className="bg-yellow-200 text-center p-2 text-sm text-black">
-        Public Layout active  {/* just for testing purposes, div to be deleted */}
-      </div>
       <main className="container mx-auto mt-4">
         <Outlet />
       </main>
