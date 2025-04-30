e9f9ae11ed0bb2418d8ea7b31a38834857b19a2e
Dimitris Bagkos <61548645+Farmakis2000@users.noreply.github.com>
2025-04-30 23:15:03 +0100

    Bug fix: Fixed timer pausing on refresh. Also auto-starts on user login.

--- frontend/src/components/private/Timer.jsx
+++ frontend/src/components/private/Timer.jsx
@@ -1,48 +1,33 @@
-import React, { useState } from "react";
-import { Button, Progress } from "flowbite-react";
+import React from "react";
+import { Progress } from "flowbite-react";
 import usePersistentTimer from "../../hooks/usePersistentTimer";
+import useAuth from "../../hooks/useAuth";
 
 function Timer() {
-  const [isLogin, setIsLogin] = useState(false);
-
+  const auth = useAuth();
   const totalTimeInSeconds = 1200;
 
   const {
     timeRemaining,
-    loginDate,
     timerMinutes,
     timerSeconds,
     resetCountdownTimer,
   } = usePersistentTimer({
-    totalTimeInSeconds: 1200,
-    isTimerActive: isLogin,
-    // onTimeRunout: () => setIsLogin(false),
+    totalTimeInSeconds,
+    isTimerActive: auth.loggedIn,
   });
 
   const progressLabel =
     100 - ((totalTimeInSeconds - timeRemaining) / totalTimeInSeconds) * 100;
 
-  const handleLogin = () => setIsLogin(true);
-  const handleLogout = () => setIsLogin(false);
-
   return (
-    <div>
+    <div className="w-full">
       <div className="flex justify-center">
         <div className="pointer-events-none absolute dark:text-white">
           {timerMinutes}:{String(timerSeconds).padStart(2, "0")}
         </div>
       </div>
-      <div class="">
-        <div>
-          <Progress progress={progressLabel} size="xl" />
-        </div>
-        <div class="inline-block">
-          <Button onClick={resetCountdownTimer}></Button>
-        </div>
-        <div class="inline-block">
-          <Button onClick={handleLogin}>Start</Button>
-        </div>
-      </div>
+      <Progress progress={progressLabel} size="xl" />
     </div>
   );
 }
