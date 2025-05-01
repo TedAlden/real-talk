9ef3a8a677259497b181de579d5d06c87d0fac64
SnBkSpy <rhickson94@gmail.com>
2025-05-01 20:55:38 +0100

    vitest files added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,9 @@ const PORT = process.env.VITE_PORT || 5002;
 export default defineConfig({
   server: { port: PORT, allowedHosts: true },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ".src/setupTests.js"
+  }
 });
