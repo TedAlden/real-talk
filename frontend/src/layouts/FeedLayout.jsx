c8eb4aadef33171d459f7cad309cdb06d2bea89b
Justin Tan <jtjtjt703@gmail.com>
2025-05-05 20:59:51 +0100

    Extracted the repsonsive feed layout to a component

---
+++ frontend/src/layouts/FeedLayout.jsx
@@ -0,0 +1,19 @@
+import Trending from "../components/Trending";
+import SuggestedUsers from "../components/SuggestedUsers";
+
+function FeedLayout({ children, viewer }) {
+  return (
+    <div className="container mx-auto">
+      <div className="grid w-full gap-6 lg:grid-cols-5 2xl:grid-cols-7">
+        <Trending className="col-span-5 hidden lg:col-span-2 lg:flex" />
+        <div className="col-span-3 text-lg text-gray-900 dark:text-white">
+          {children}
+        </div>
+        <div className="col-span-5 flex hidden w-full 2xl:col-span-2 2xl:flex">
+          <SuggestedUsers viewer={viewer} />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default FeedLayout;
