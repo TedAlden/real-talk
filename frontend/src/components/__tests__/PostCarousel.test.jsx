1ffd37ba26eb27f9ba44bab5fd095d92ae3012d9
SnBkSpy <rhickson94@gmail.com>
2025-05-05 19:19:43 +0100

    Improved unit tests

--- frontend/src/components/__tests__/PostCarousel.test.jsx
+++ frontend/src/components/__tests__/PostCarousel.test.jsx
@@ -13,11 +13,15 @@ describe("PostCarousel", () => {
     render(<PostCarousel images={image_map} />);
     const images = screen.getAllByRole("img");
     expect(images).toHaveLength(1);
+    expect(images).not.toHaveLength(0);
+    expect(images).not.toHaveLength(2);
   });
-  const image_map_2 = ["../../assets/test.webp", "../../assets/test.webp", "../../assets/test.webp"];
-  it("displays a single image when given a single image to display", () => {
+  const image_map_2 = ["../../assets/test.webp", "../../assets/test.webp", "../../assets/test.webp"]; // 3 images
+  it("displays the correct number of images if given an image map of more than one image", () => {
     render(<PostCarousel images={image_map_2} />);
     const images = screen.getAllByRole("img");
     expect(images).toHaveLength(3);
+    expect(images).not.toHaveLength(2);
+    expect(images).not.toHaveLength(4); // Five is right out
   });
 });
