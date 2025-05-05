a2fb258b8247ec831b5d3764f854ed0242e66f98
SnBkSpy <rhickson94@gmail.com>
2025-05-05 19:32:25 +0100

    Alert unit test

---
+++ frontend/src/components/__tests__/Alert.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { describe, vitest } from "vitest";
+import Alert from "../Alert"
+
+describe("Alert", () => {
+    it("displays title and message", () => {
+        render(<Alert show={"oui oui, hon hon baguette"} title={"alert_title"} message={"alert_message"} />);
+        expect(screen.getByText("alert_title")).toBeInTheDocument();
+        expect(screen.getByText("alert_message")).toBeInTheDocument();
+    })
+})
